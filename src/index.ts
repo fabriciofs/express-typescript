a90fd918a6778a10ba0e030097b55e0618e55856
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2021-09-15 12:01:10 -0300

    refactor to use controller and router

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, { Application } from 'express';
 import morgan from 'morgan';
+import Router from './routes';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
 
@@ -9,9 +10,7 @@ app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.static('public'));
 
-app.get('/ping', async (req, res) => {
-  res.send({ message: 'pong' });
-});
+app.use(Router);
 
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
